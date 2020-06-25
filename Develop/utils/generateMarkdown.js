cb26ad9b875f4b1253f426ea58f094716794a9d6
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-06-25 07:08:44 -0400

    titles added to generalMarkdowm function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description 
+  ${data.title}
+
+  ## Table of Contents (Optional)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Badges](#badges)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+
+  ## Usage 
+
+  ## Credits
+
+  ## License
+
+  ## Badges
+
+  ## Contributing
+
+  ## Tests
+
+
+
+
+
 
 `;
 }
